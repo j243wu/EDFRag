5b39afd9d24d52aab424b0eb71b749c961a85ea6
Jize Wu <j243wu@uwaterloo.ca>
2025-10-03 23:04:11 -0400

    package json update

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,8 @@ const nextConfig = {
   output: 'export', // enables `next export` for static hosting
   images: { unoptimized: true },
 };
-export default nextConfig;
+export default {
+  output: 'export',
+  images: { unoptimized: true },
+};
+
